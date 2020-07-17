283863a63f609ab68174e174e1e58c50b02936d2
Wesley Rocha <wesley.rocha@bbts.com.br>
2020-07-17 12:53:40 -0300

    add git ignore

--- src/main/java/conversor/Aplicacao.java
+++ src/main/java/conversor/Aplicacao.java
@@ -5,7 +5,6 @@ public class Aplicacao {
     public static void main(String[] args) {
     	String caminho = "";
     	
-    	//caminho = "/home/wesley/staff.xml";
     	caminho = "/home/wesley/Downloads/CVM/arquivos/valerio/noticias/arquivos/2019/20190104-1.html"; 
     	
         ImprimeXMLnoConsole imp = new ImprimeXMLnoConsole();
